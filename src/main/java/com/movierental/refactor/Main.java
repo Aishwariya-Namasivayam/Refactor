3850c0a625a9eead1fb4149ff8b5aa58e84ec2fe
NAMASIVAYAM Aishwariya <anamasivayam@altran.com>
2019-11-15 17:22:02 +0100

    Commit for Movie Rentals Code Refactoring test

---
+++ src/main/java/com/movierental/refactor/Main.java
@@ -0,0 +1,20 @@
+package com.movierental.refactor;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+
+    Customer customer = new Customer("C. U. Stomer");
+    customer.addRental(new MovieRental(new Movie("You've Got Mail", Movie.REGULAR),3));
+    customer.addRental(new MovieRental(new Movie("Matrix", Movie.REGULAR),1));
+    
+    String result = customer.statement();
+    
+    if (!result.equals(expected)) {
+      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+    }
+
+    System.out.println("Success");
+  }
+}
